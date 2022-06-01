e8876ff8da6a2137a27879fb5ee10be64c68329e
imikado@gmail.com <imikado@gmail.com>
2022-06-01 21:38:47 +0200

    init

---
+++ src/main/kotlin/com/mika/tutorialKotlinGraphQL/TutorialKotlinGraphQlApplication.kt
@@ -0,0 +1,11 @@
+package com.mika.tutorialKotlinGraphQL
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TutorialKotlinGraphQlApplication
+
+fun main(args: Array<String>) {
+	runApplication<TutorialKotlinGraphQlApplication>(*args)
+}
---
+++ src/test/kotlin/com/mika/tutorialKotlinGraphQL/TutorialKotlinGraphQlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mika.tutorialKotlinGraphQL
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TutorialKotlinGraphQlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
