69ec3693cd9c47e48cc8356afefa810d29101284
Antonio Gonzalez <agonzalez@quantificare.com>
2022-12-07 14:49:39 +0100

    author update fix

--- src/main/kotlin/com/mika/tutorialKotlinGraphQL/graphql/types/input/AuthorUpdate.kt
+++ src/main/kotlin/com/mika/tutorialKotlinGraphQL/graphql/types/input/AuthorUpdate.kt
@@ -2,8 +2,6 @@ package com.mika.tutorialKotlinGraphQL.graphql.types.input
 
 data class AuthorUpdate (
     var id: Long,
-    val title: String?,
-    val author_id: Long?,
-    val category_id: Long?
-
+    val firstname: String?,
+    val lastname: Long?,
 )
